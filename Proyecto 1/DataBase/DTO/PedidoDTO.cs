99eedb050369764afe7210db4a8c8eec50c6f116
Valentin <valentinale203@gmail.com>
2023-10-13 12:22:12 -0300

    Modificacion de PedidoDTO

--- Proyecto 1/DataBase/DTO/PedidoDTO.cs
+++ Proyecto 1/DataBase/DTO/PedidoDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Proyecto_1.Modelo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace Proyecto_1.DTO
 {
     internal class PedidoDTO
     {
+        public int NumeroPedido { get; set; }
+        public List<Taco> ListaTacos { get; set; }
+        public int PrecioToTalPedidos { get; set; }
+
     }
 }
